f5025df0022fe6e9476205b6472c0a8b3b7c64ef
JoProvost <jprovost.sh@gmail.com>
2016-12-07 12:45:24 -0500

    Day 3: Squares With Three Sides (Part 2)

--- src/square_with_three_sides.py
+++ src/square_with_three_sides.py
@@ -1,14 +1,28 @@
-def parse_sides(line):
-    return tuple(map(int, line.split()))
+def parse_rows(text):
+    return (tuple(map(int, row.split()))
+            for row in text.splitlines())
 
 
-def is_valid_triangle(line):
-    sides = parse_sides(line)
-    if len(sides) != 3:
-        return False
-    a, b, c = sorted(sides)
-    return (a + b) > c
+def parse_columns(text):
+    rows = tuple(parse_rows(text))
+
+    for row_number in range(0, len(rows), 3):
+        for triangle_index in range(len(rows[row_number])):
+            yield (
+                rows[row_number][triangle_index],
+                rows[row_number + 1][triangle_index],
+                rows[row_number + 2][triangle_index])
 
 
-def how_many_valid_triangles(triangle_list_text):
-    return len(tuple(filter(is_valid_triangle, triangle_list_text.splitlines())))
+def is_valid_triangle(sides):
+    try:
+        a, b, c = sorted(sides)
+        return (a + b) > c
+    except ValueError:
+        pass
+
+
+def how_many_valid_triangles(triangles):
+    return len(tuple(filter(is_valid_triangle, triangles)))
+
+
--- test/test_square_with_three_sides.py
+++ test/test_square_with_three_sides.py
@@ -29,24 +29,86 @@ class TestPart1(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_2_sides_triangle_is_invalid(self):
-        self.assertEqual(0, square_with_three_sides.how_many_valid_triangles('1 2'))
+    def test_parsing_rows(self):
+        self.assertEqual(
+            ((1, 2, 3), (4, 5, 6)),
+            tuple(square_with_three_sides.parse_rows('1 2 3\n4 5 6')))
 
-    def test_3_sides_triangle_is_valid(self):
-        self.assertEqual(1, square_with_three_sides.how_many_valid_triangles('2 3 4'))
+    def test_with_a_valid_triangle(self):
+        self.assertEqual(1, square_with_three_sides.how_many_valid_triangles(
+            square_with_three_sides.parse_rows('2 3 4')))
 
     def test_with_two_valid_triangles(self):
-        self.assertEqual(2, square_with_three_sides.how_many_valid_triangles('2 3 4\n4 4 4'))
+        self.assertEqual(2, square_with_three_sides.how_many_valid_triangles(
+            square_with_three_sides.parse_rows('2 3 4\n4 4 4')))
 
     def test_sum_of_any_two_sides_must_be_larger_than_the_remaining_side(self):
-        self.assertEqual(0, square_with_three_sides.how_many_valid_triangles('2 2 4'))
-        self.assertEqual(0, square_with_three_sides.how_many_valid_triangles('2 4 2'))
-        self.assertEqual(0, square_with_three_sides.how_many_valid_triangles('4 2 2'))
-        self.assertEqual(1, square_with_three_sides.how_many_valid_triangles('2 3 4'))
-        self.assertEqual(1, square_with_three_sides.how_many_valid_triangles('4 2 3'))
-        self.assertEqual(1, square_with_three_sides.how_many_valid_triangles('2 4 3'))
+        self.assertEqual(0, square_with_three_sides.how_many_valid_triangles(
+            square_with_three_sides.parse_rows('2 2 4')))
+        self.assertEqual(0, square_with_three_sides.how_many_valid_triangles(
+            square_with_three_sides.parse_rows('2 4 2')))
+        self.assertEqual(0, square_with_three_sides.how_many_valid_triangles(
+            square_with_three_sides.parse_rows('4 2 2')))
+        self.assertEqual(1, square_with_three_sides.how_many_valid_triangles(
+            square_with_three_sides.parse_rows('2 3 4')))
+        self.assertEqual(1, square_with_three_sides.how_many_valid_triangles(
+            square_with_three_sides.parse_rows('4 2 3')))
+        self.assertEqual(1, square_with_three_sides.how_many_valid_triangles(
+            square_with_three_sides.parse_rows('2 4 3')))
 
     def test_puzzle_input(self):
         with open(join(dirname(__file__), 'resources', 'square_with_three_sides.txt')) as f:
             puzzle_input = f.read()
-        self.assertEqual(869, square_with_three_sides.how_many_valid_triangles(puzzle_input))
+        self.assertEqual(869, square_with_three_sides.how_many_valid_triangles(
+            square_with_three_sides.parse_rows(puzzle_input)))
+
+
+class TestPart2(unittest.TestCase):
+    """
+    --- Part Two ---
+
+    Now that you've helpfully marked up their design documents, it occurs to
+    you that triangles are specified in groups of three vertically. Each set
+    of three numbers in a column specifies a triangle. Rows are unrelated.
+
+    For example, given the following specification, numbers with the same
+    hundreds digit would be part of the same triangle:
+
+    101 301 501
+    102 302 502
+    103 303 503
+    201 401 601
+    202 402 602
+    203 403 603
+
+    In your puzzle input, and instead reading by columns, how many of the
+    listed triangles are possible?
+    """
+
+    def setUp(self):
+        pass
+
+    def test_parsing_columns(self):
+        self.assertEqual(
+            ((1, 2, 3), (4, 5, 6), (7, 8, 9)),
+            tuple(square_with_three_sides.parse_columns(
+                '1 4 7\n'
+                '2 5 8\n'
+                '3 6 9')))
+
+    def test_column_based_parsing(self):
+        self.assertEqual(6, square_with_three_sides.how_many_valid_triangles(
+            square_with_three_sides.parse_columns(
+                '101 301 501\n'
+                '102 302 502\n'
+                '103 303 503\n'
+                '201 401 601\n'
+                '202 402 602\n'
+                '203 403 603'
+            )))
+
+    def test_puzzle_input(self):
+        with open(join(dirname(__file__), 'resources', 'square_with_three_sides.txt')) as f:
+            puzzle_input = f.read()
+        self.assertEqual(1544, square_with_three_sides.how_many_valid_triangles(
+            square_with_three_sides.parse_columns(puzzle_input)))
