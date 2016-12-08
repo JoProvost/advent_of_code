167a3089818b4f7d658896ae9ffba4c9ba863547
JoProvost <jprovost.sh@gmail.com>
2016-12-08 12:00:11 -0500

    Day 7: Internet Protocol Version 7 (Part 2)

--- src/internet_protocol_version_7.py
+++ src/internet_protocol_version_7.py
@@ -1,11 +1,35 @@
-def find_abba(address):
-    for position in range(len(address) - 3):
-        a, b, c, d = address[position:position + 4]
-        if (a, b) == (d, c) and a != b:
-            yield position
+import collections
+
+Detection = collections.namedtuple(
+    'Detection', 'pos text inv_text in_brackets')
 
 
-def is_in_brackets(position, address):
+def _detect_palindromes(size, address):
+    for pos in range(len(address) - (size - 1)):
+        text = address[pos:pos + size]
+        if _valid_palindrome(text):
+            yield Detection(pos, text, _reverse_palindrome(text), _in_brackets(pos, address))
+
+
+def _valid_palindrome(palindrome):
+    if palindrome == palindrome[0] * len(palindrome):
+        return False
+    for i in range(len(palindrome)):
+        if palindrome[i] != palindrome[-i-1]:
+            return False
+    return True
+
+
+def _reverse_palindrome(palindrome):
+    if len(palindrome) == 4:
+        a, b, c, d = palindrome
+        return ''.join((b, a, d, c))
+    if len(palindrome) == 3:
+        a, b, c = palindrome
+        return ''.join((b, a, b))
+
+
+def _in_brackets(position, address):
     bracket_end = address.find(']', position)
     if bracket_end == -1:
         return False
@@ -16,14 +40,27 @@ def is_in_brackets(position, address):
 
 
 def supports_tls(address):
-    abba_positions = tuple(find_abba(address))
-    if len(abba_positions) == 0:
+    detections = tuple(_detect_palindromes(4, address))
+    if len(detections) == 0:
         return False
-    for position in abba_positions:
-        if is_in_brackets(position, address):
+    for detection in detections:
+        if detection.in_brackets:
             return False
     return True
 
 
+def supports_ssl(address):
+    detections = tuple(_detect_palindromes(3, address))
+    for bab in detections:
+        for aba in detections:
+            if bab.text == aba.inv_text and bab.in_brackets != aba.in_brackets:
+               return True
+    return False
+
+
 def how_many_addresses_suporting_tls(addresses):
     return len([address for address in addresses.splitlines() if supports_tls(address)])
+
+
+def how_many_addresses_suporting_ssl(addresses):
+    return len([address for address in addresses.splitlines() if supports_ssl(address)])
--- test/test_internet_protocol_version_7.py
+++ test/test_internet_protocol_version_7.py
@@ -55,3 +55,59 @@ class TestPart1(unittest.TestCase):
         with open(join(dirname(__file__), 'resources', 'internet_protocol_version_7.txt')) as f:
             puzzle_input = f.read()
         self.assertEqual(118, internet_protocol_version_7.how_many_addresses_suporting_tls(puzzle_input))
+
+
+class TestPart2(unittest.TestCase):
+    """
+    --- Part Two ---
+
+    You would also like to know which IPs support SSL (super-secret
+    listening).
+
+    An IP supports SSL if it has an Area-Broadcast Accessor, or ABA, anywhere
+    in the supernet sequences (outside any square bracketed sections), and a
+    corresponding Byte Allocation Block, or BAB, anywhere in the hypernet
+    sequences. An ABA is any three-character sequence which consists of the
+    same character twice with a different character between them, such as xyx
+    or aba. A corresponding BAB is the same characters but in reversed
+    positions: yxy and bab, respectively.
+
+    For example:
+
+    - aba[bab]xyz supports SSL (aba outside square brackets with
+      corresponding bab within square brackets).
+    - xyx[xyx]xyx does not support SSL (xyx, but no corresponding yxy).
+    - aaa[kek]eke supports SSL (eke in supernet with corresponding kek in
+      hypernet; the aaa sequence is not related, because the interior
+      character must be different).
+    - zazbz[bzb]cdb supports SSL (zaz has no corresponding aza, but zbz has
+      a corresponding bzb, even though zaz and zbz overlap).
+
+    How many IPs in your puzzle input support SSL?
+    """
+
+    def test_address_without_any_bab_does_not_support_ssl(self):
+        self.assertFalse(internet_protocol_version_7.supports_ssl('abc[def]ghi'))
+
+    def test_bab_outside_brackes_with_aba_inside_brackets_supports_ssl(self):
+        self.assertTrue(internet_protocol_version_7.supports_ssl('bab[aba]qrst'))
+        self.assertTrue(internet_protocol_version_7.supports_ssl('qrst[aba]bab'))
+
+    def test_aaa_does_not_support_ssl(self):
+        self.assertFalse(internet_protocol_version_7.supports_ssl('aaa[aaa]ghi'))
+
+    def test_overlapping_bab_outside_brackes_with_aba_inside_brackets_supports_ssl(self):
+        self.assertTrue(internet_protocol_version_7.supports_ssl('zazbz[bzb]cdb'))
+
+    def test_how_many_addresses_suporting_ssl(self):
+        self.assertEqual(3, internet_protocol_version_7.how_many_addresses_suporting_ssl(
+            'aba[bab]xyz\n'  # yes
+            'xyx[xyx]xyx\n'  # no
+            'aaa[kek]eke\n'  # yes
+            'zazbz[bzb]cdb'  # yes
+        ))
+
+    def test_puzzle_input(self):
+        with open(join(dirname(__file__), 'resources', 'internet_protocol_version_7.txt')) as f:
+            puzzle_input = f.read()
+        self.assertEqual(260, internet_protocol_version_7.how_many_addresses_suporting_ssl(puzzle_input))
