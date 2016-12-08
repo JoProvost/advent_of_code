2db07062910881b3970c8a5323d17061be63e3b9
JoProvost <jprovost.sh@gmail.com>
2016-12-07 23:11:28 -0500

    Day 6: Signals and Noise (Part 2)

--- src/signals_and_noise.py
+++ src/signals_and_noise.py
@@ -1,7 +1,13 @@
 from collections import Counter
 
 
-def error_correction(signal_text):
+def error_corrected_message(signal_text):
     transmissions = signal_text.splitlines()
     return ''.join(Counter(repeated_character).most_common()[0][0]
+                   for repeated_character in  zip(*transmissions))
+
+
+def hidden_message(signal_text):
+    transmissions = signal_text.splitlines()
+    return ''.join(Counter(repeated_character).most_common()[-1][0]
                    for repeated_character in  zip(*transmissions))
--- test/test_signals_and_noise.py
+++ test/test_signals_and_noise.py
@@ -48,7 +48,7 @@ class TestPart1(unittest.TestCase):
     """
 
     def test_error_correction(self):
-        valid_message = signals_and_noise.error_correction(
+        valid_message = signals_and_noise.error_corrected_message(
             'eedadn\n'
             'drvtee\n'
             'eandsr\n'
@@ -70,5 +70,51 @@ class TestPart1(unittest.TestCase):
     def test_puzzle_input(self):
         with open(join(dirname(__file__), 'resources', 'signals_and_noise.txt')) as f:
             puzzle_input = f.read()
-        self.assertEqual('ikerpcty', signals_and_noise.error_correction(puzzle_input))
+        self.assertEqual('ikerpcty', signals_and_noise.error_corrected_message(puzzle_input))
 
+
+class TestPart2(unittest.TestCase):
+    """
+    --- Part Two ---
+
+    Of course, that would be the message - if you hadn't agreed to use a
+    modified repetition code instead.
+
+    In this modified code, the sender instead transmits what looks like
+    random data, but for each character, the character they actually want to
+    send is slightly less likely than the others. Even after signal-jamming
+    noise, you can look at the letter distributions in each column and choose
+    the least common letter to reconstruct the original message.
+
+    In the above example, the least common character in the first column is a;
+    in the second, d, and so on. Repeating this process for the remaining
+    characters produces the original message, advent.
+
+    Given the recording in your puzzle input and this new decoding
+    methodology, what is the original message that Santa is trying to send?
+    """
+
+    def test_error_correction(self):
+        valid_message = signals_and_noise.hidden_message(
+            'eedadn\n'
+            'drvtee\n'
+            'eandsr\n'
+            'raavrd\n'
+            'atevrs\n'
+            'tsrnev\n'
+            'sdttsa\n'
+            'rasrtv\n'
+            'nssdts\n'
+            'ntnada\n'
+            'svetve\n'
+            'tesnvt\n'
+            'vntsnd\n'
+            'vrdear\n'
+            'dvrsen\n'
+            'enarar\n')
+        self.assertEqual('advent', valid_message)
+
+    def test_puzzle_input(self):
+        with open(join(dirname(__file__), 'resources', 'signals_and_noise.txt')) as f:
+            puzzle_input = f.read()
+        self.assertEqual('uwpfaqrq', signals_and_noise.hidden_message(puzzle_input))
