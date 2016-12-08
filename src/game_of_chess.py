690fa46359ea53aab0ddee0d97d7caa174bd1c19
JoProvost <jprovost.sh@gmail.com>
2016-12-07 22:05:56 -0500

    Day 5: How About a Nice Game of Chess? (Part 2)

--- src/game_of_chess.py
+++ src/game_of_chess.py
@@ -1,14 +1,31 @@
 import hashlib
 
 
-def get_password_for_door(door_id):
+def first_algorithm(door_id):
     password = ''
     index = 0
 
     while len(password) < 8:
-        hexdigest = hashlib.md5((door_id + str(index)).encode('utf-8')).hexdigest()
-        if hexdigest[:5] == '00000':
-            password += hexdigest[5]
+        digest = hashlib.md5((door_id + str(index)).encode('utf-8')).hexdigest()
+        if digest[:5] == '00000':
+            password += digest[5]
         index += 1
 
     return password
+
+
+def second_algorithm(door_id):
+    password = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    index = 0
+
+    while ' ' in password:
+        digest = hashlib.md5((door_id + str(index)).encode('utf-8')).hexdigest()
+        if digest[:5] == '00000':
+            try:
+                if password[int(digest[5], 16)] == ' ':
+                    password[int(digest[5], 16)] = digest[6]
+            except IndexError:
+                pass
+        index += 1
+
+    return ''.join(password)
