9425e45212b76755a67d6278156f3ddf2fba9109
JoProvost <jprovost.sh@gmail.com>
2016-12-23 12:21:08 -0500

    Day 17: Two Steps Forward (part 1)

---
+++ src/two_steps_forward.py
@@ -0,0 +1,82 @@
+import hashlib
+import operator
+
+
+class Path(object):
+
+    start_location = (0, 0)
+    moves = {
+        'U': (0, -1),
+        'D': (0, +1),
+        'L': (-1, 0),
+        'R': (+1, 0),
+    }
+
+    def __init__(self, path=''):
+        self._path = path
+
+    @property
+    def location(self):
+        l = self.start_location
+        for d in self._path:
+            l = move(l, self.moves[d])
+        return l
+
+    def next(self, dimensions, open_doors):
+        actual_location = self.location
+        return tuple(
+            Path(self._path + d) for d in self.moves
+            if in_bounds(move(actual_location, self.moves[d]), dimensions) and d in open_doors)
+
+    def __str__(self):
+        return self._path
+
+    def __len__(self):
+        return len(self._path)
+
+
+class DisabledSecurity(object):
+    def open_doors(self, path):
+        return 'UDLR'
+
+
+class EasterBunnyVaultSecurity(object):
+    doors = 'UDLR'
+
+    def __init__(self, passcode):
+        self.passcode = passcode
+
+    def open_doors(self, path):
+        md5_hash = hashlib.md5('{}{}'.format(self.passcode, path).encode('ascii'))
+        locked = md5_hash.hexdigest()[:4]
+        return ''.join(door for i, door in enumerate(self.doors) if locked[i] in 'bcdef')
+
+
+class Maze(object):
+
+    def __init__(self, security, width=4, height=4):
+        self.security = security
+        self.dimensions = (width, height)
+
+    def shortest_path_to(self, x, y):
+        destination = (x, y)
+        paths = (Path(),)
+
+        while paths:
+            next_paths = []
+            for path in paths:
+                if path.location == destination:
+                    return path
+                open_doors = self.security.open_doors(path)
+                next_paths.extend(path.next(self.dimensions, open_doors))
+            paths = tuple(next_paths)
+        raise StopIteration()
+
+
+def move(location, offset):
+    return location[0] + offset[0], location[1] + offset[1]
+
+
+def in_bounds(location, dimensions):
+    return all(map(operator.lt, location, dimensions)) and \
+           all(map(operator.ge, location, (0, 0)))
---
+++ test/test_two_steps_forward.py
@@ -0,0 +1,103 @@
+import unittest
+from two_steps_forward import *
+
+
+class TestPart1(unittest.TestCase):
+    """
+    --- Day 17: Two Steps Forward ---
+
+    You're trying to access a secure vault protected by a 4x4 grid of small
+    rooms connected by doors. You start in the top-left room (marked S), and
+    you can access the vault (marked V) once you reach the bottom-right room:
+
+    #########
+    #S| | | #
+    #-#-#-#-#
+    # | | | #
+    #-#-#-#-#
+    # | | | #
+    #-#-#-#-#
+    # | | |
+    ####### V
+
+    Fixed walls are marked with #, and doors are marked with - or |.
+
+    The doors in your current room are either open or closed (and locked)
+    based on the hexadecimal MD5 hash of a passcode (your puzzle input)
+    followed by a sequence of uppercase characters representing the path you
+    have taken so far (U for up, D for down, L for left, and R for right).
+
+    Only the first four characters of the hash are used; they represent,
+    respectively, the doors up, down, left, and right from your current
+    position. Any b, c, d, e, or f means that the corresponding door is open;
+    any other character (any number or a) means that the corresponding door
+    is closed and locked.
+
+    To access the vault, all you need to do is reach the bottom-right room;
+    reaching this room opens the vault and all doors in the maze.
+
+    For example, suppose the passcode is hijkl. Initially, you have taken no
+    steps, and so your path is empty: you simply find the MD5 hash of hijkl
+    alone. The first four characters of this hash are ced9, which indicate
+    that up is open (c), down is open (e), left is open (d), and right is
+    closed and locked (9). Because you start in the top-left corner, there
+    are no "up" or "left" doors to be open, so your only choice is down.
+
+    Next, having gone only one step (down, or D), you find the hash of hijklD.
+    This produces f2bc, which indicates that you can go back up, left (but
+    that's a wall), or right. Going right means hashing hijklDR to get 5745 -
+    all doors closed and locked. However, going up instead is worthwhile:
+    even though it returns you to the room you started in, your path would
+    then be DU, opening a different set of doors.
+
+    After going DU (and then hashing hijklDU to get 528e), only the right
+    door is open; after going DUR, all doors lock. (Fortunately, your actual
+    passcode is not hijkl).
+
+    Passcodes actually used by Easter Bunny Vault Security do allow access to
+    the vault if you know the right path. For example:
+
+    - If your passcode were ihgpwlah, the shortest path would be DDRRRD.
+    - With kglvqrro, the shortest path would be DDUDRLRRUDRD.
+    - With ulqzkmiv, the shortest would be DRURDRUDDLLDLUURRDULRLDUUDDDRR.
+
+    Given your vault's passcode, what is the shortest path (the actual path,
+    not just the length) to reach the vault?
+    """
+
+    def test_shortest_path_on_all_open_doors(self):
+        security = DisabledSecurity()
+        maze = Maze(security)
+        path = maze.shortest_path_to(3, 3)
+        self.assertEqual(6, len(path))
+
+    def test_easter_bunny_vault_security(self):
+        security = EasterBunnyVaultSecurity('hijkl')
+        self.assertEqual('UDL', security.open_doors(Path()))
+        self.assertEqual('ULR', security.open_doors(Path('D')))
+        self.assertEqual('', security.open_doors(Path('DR')))
+        self.assertEqual('R', security.open_doors(Path('DU')))
+
+    def test_example_1(self):
+        security = EasterBunnyVaultSecurity('ihgpwlah')
+        maze = Maze(security)
+        path = maze.shortest_path_to(3, 3)
+        self.assertEqual('DDRRRD', str(path))
+
+    def test_example_2(self):
+        security = EasterBunnyVaultSecurity('kglvqrro')
+        maze = Maze(security)
+        path = maze.shortest_path_to(3, 3)
+        self.assertEqual('DDUDRLRRUDRD', str(path))
+
+    def test_example_3(self):
+        security = EasterBunnyVaultSecurity('ulqzkmiv')
+        maze = Maze(security)
+        path = maze.shortest_path_to(3, 3)
+        self.assertEqual('DRURDRUDDLLDLUURRDULRLDUUDDDRR', str(path))
+
+    def test_puzzle(self):
+        security = EasterBunnyVaultSecurity('vkjiggvb')
+        maze = Maze(security)
+        path = maze.shortest_path_to(3, 3)
+        self.assertEqual('RDRRULDDDR', str(path))
